516c34efcbab5766f13a30d276072f50e85fb7fe
Roque Armando Ramirez Pionce <109245164+Armando8bits@users.noreply.github.com~>
2023-12-06 20:44:27 -0500

    inicia en incognito para evitar navegación en redes con restricciones, ya que evadoverificacion SSL

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from src.Acciones import orquestador
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService #para no estar descargando el chromedriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 if credencial.Credencial.EsValida(): #si las credenciales son validas, inicia
-    #print(str(Credencial.GetUsuario())+" - "+str(Credencial.GetPasswor()))
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    options = Options()
+    options.add_argument("--incognito")
+    '''El perfil de Chrome "Incognito" no verifica los certificados SSL por defecto. Esta opción es más segura que la anterior,
+    ya que el perfil de incognito no guarda cookies ni historial de navegación. en lugar de usar el argumento "--ignore-certificate-errors"
+    al inicio de Chrome, lo que le indicará que ignore cualquier error relacionado con los certificados SSL. pero este último no es recomendado
+    ya que una práctica insegura y no se recomienda, ya que expone su conexión a posibles ataques y robo de datos. '''
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     driver.maximize_window() #creo que es obvio lo que hace
     driver.get("https://servicioenlinea.ug.edu.ec/SIUG/Account/Login.aspx")
 
