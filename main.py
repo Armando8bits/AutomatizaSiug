0c6138cd4ffeeb2429ac769c91a8d09c579914f7
Roque Armando Ramírez <109245164+Armando8bits@users.noreply.github.com>
2024-07-27 12:47:06 -0500

    se agrega manejo de errores, para evitar que se caiga cuando un equipo tiene problemas de conexión a internet, permitiendo reintentar la ejecución

--- main.py
+++ main.py
@@ -3,28 +3,43 @@ from src.Acciones import login, credencial
 from src.Acciones import orquestador
 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.service import Service as ChromeService #para no estar descargando el chromedriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-if credencial.Credencial.EsValida(): #si las credenciales son validas, inicia
-    options = Options()
-    options.add_argument("--incognito")
-    '''El perfil de Chrome "Incognito" no verifica los certificados SSL por defecto. Esta opción es más segura que la anterior,
-    ya que el perfil de incognito no guarda cookies ni historial de navegación. en lugar de usar el argumento "--ignore-certificate-errors"
-    al inicio de Chrome, lo que le indicará que ignore cualquier error relacionado con los certificados SSL. pero este último no es recomendado
-    ya que una práctica insegura y no se recomienda, ya que expone su conexión a posibles ataques y robo de datos. '''
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    driver.maximize_window() #creo que es obvio lo que hace
-    driver.get("https://servicioenlinea.ug.edu.ec/SIUG/Account/Login.aspx")
+while True:
+    try:
+        if credencial.Credencial.EsValida(): #si las credenciales son validas, inicia
+            options = Options()
+            options.add_argument("--incognito")
+            '''El perfil de Chrome "Incognito" no verifica los certificados SSL por defecto. Esta opción es más segura que la anterior,
+            ya que el perfil de incognito no guarda cookies ni historial de navegación. en lugar de usar el argumento "--ignore-certificate-errors"
+            al inicio de Chrome, lo que le indicará que ignore cualquier error relacionado con los certificados SSL. pero este último no es recomendado
+            ya que una práctica insegura y no se recomienda, ya que expone su conexión a posibles ataques y robo de datos. '''
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+            driver.maximize_window() #creo que es obvio lo que hace
+            driver.get("https://servicioenlinea.ug.edu.ec/SIUG/Account/Login.aspx")
 
-    login.Sesion().iniciar(driver,credencial.Credencial.GetUsuario(),
-                           credencial.Credencial.GetPasswor(),
-                           credencial.Credencial.GetFecme(),
-                           credencial.Credencial.GetFecdi()     )
-    orquestador.Orquestador().Dirige(driver)
-    input("***Presiona ENTER para cerrar el navegador...") #para evitar que se cierre al culminar el script
-    driver.quit()
+            login.Sesion().iniciar(driver,credencial.Credencial.GetUsuario(),
+                                credencial.Credencial.GetPasswor(),
+                                credencial.Credencial.GetFecme(),
+                                credencial.Credencial.GetFecdi()     )
+            orquestador.Orquestador().Dirige(driver)
+            input("***Presiona ENTER para cerrar el navegador...") #para evitar que se cierre al culminar el script
+            driver.quit()
+            # Si todas las funciones se ejecutan correctamente, sal del bucle
+            break
 
+    except Exception as e:
+        if "Are you offline?" in str(e):
+            print("Error: Verifica si hay conexión a Internet...")
+        else:
+            print(f"Ocurrió un error inesperado: {e}")
+            print(f"Tipo de excepción: {type(e)}")
+        retry = input("¿Desea volver a reintentar? (S/N): ").strip().lower()
+        if retry != 's':
+            print("Saliendo del programa.")
+            break
 
 
